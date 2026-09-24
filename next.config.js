7b7bb42ae5645283002bc5f4c9665f35c843e51f
mrhan97official <mrhan97.official@gmail.com>
2026-09-24 09:28:09 +0700

    DevControl: Update 1

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withPWA = require("next-pwa")({
     {
       urlPattern: /^\/api\/.*$/,
       handler: "NetworkOnly",
+      options: {},
     },
     {
       urlPattern: /^(?!.*\/api\/).*/,
